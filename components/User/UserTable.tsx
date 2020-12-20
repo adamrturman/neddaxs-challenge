609637c4d517364bb4d496abe3c89809d76d4650
adamrturman <adamrturman@gmail.com>
2020-12-20 16:29:09 -0600

    single user view now uses mui table

--- components/User/UserTable.tsx
+++ components/User/UserTable.tsx
@@ -1,20 +1,44 @@
 import UserRow from '../Users/UserRow';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 const UserTable = (props) => {
   const { user, id } = props;
   console.log("this is user", user)
   console.log("this is id", id)
 
+  if(!user.id) {
+      return <h1>This user does not exist!</h1>
+  }
+  
   return (
-    <table style={{ width: "100%", height: "20rem", marginRight: "2rem", borderCollapse: "collapse", marginTop: "10rem" }}>
-      <tbody>
-        <th>Id: {user.id}</th>
-        <th>First name: {user.firstName}</th>
-        <th>Last name: {user.lastName}</th>
-        <th>Email: {user.email}</th>
-        <th>Created at: {user.createdAt}</th>
-      </tbody>
-    </table>
+    <TableContainer component={Paper}>
+      <Table aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>User ID</TableCell>
+            <TableCell align="right">First Name</TableCell>
+            <TableCell align="right">Last Name</TableCell>
+            <TableCell align="right">Email</TableCell>
+            <TableCell align="right">Date added</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+            <TableRow key={user.id}>
+              <TableCell align="right">{user.id}</TableCell>
+              <TableCell align="right">{user.firstName}</TableCell>
+              <TableCell align="right">{user.lastName}</TableCell>
+              <TableCell align="right">{user.email}</TableCell>
+              <TableCell align="right">{user.createdAt}</TableCell>
+            </TableRow>      
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
 
